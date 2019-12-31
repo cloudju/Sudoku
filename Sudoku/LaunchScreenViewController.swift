84df9a2546f50f54074e4dc41be46072ab73a330
cloudju <cloudju1978@gmail.com>
2019-12-31 22:39:17 +0900

    快速略过LaunchScreen

---
+++ Sudoku/LaunchScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchScreenViewController.swift
+//  Sudoku
+//
+//  Created by 居朝強 on 2019/12/31.
+//  Copyright © 2019 Cloud.Ju. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
